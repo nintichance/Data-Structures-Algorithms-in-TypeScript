5502f15e1dbc9f380f435593183e5fa66fe240d5
Ninti Chance <mintidance@gmail.com>
2025-09-18 22:49:02 -0400

    Add semicolons

--- binarysearch/implementation.ts
+++ binarysearch/implementation.ts
@@ -6,14 +6,10 @@ function search(nums: number[], target: number): number {
         midd = Math.floor((left + right)/2)
         if (nums[midd] === target) return midd;
         else if (nums[midd]> target) {
-            right = midd - 1
+            right = midd - 1;
         } else {
-            left = midd + 1
+            left = midd + 1;
         }
     }
-    return -1
+    return -1;
 }
-
-const list = [1,2,3,4,5,6,7,8,9,10]
-const numToFind = 8
-console.log("WHERE IS NUM IN LIST", search(list, numToFind))
